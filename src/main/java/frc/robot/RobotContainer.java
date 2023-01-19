76a6c5144868d4599349b94a33ef6017abb7b3dc
ajmoritz2 <chickenville.trade.head@gmail.com>
2023-01-19 18:43:52 -0500

    first commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,112 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.*;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.loops.SubsystemManager;
+
+
+import java.nio.file.Path;
+
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+
+	private static RobotContainer INSTANCE;
+
+	public final XboxController driverController;
+	public final XboxController operatorController;
+
+	private final SubsystemManager manager;
+
+	private SendableChooser<Command> autonChooser;
+
+	public enum EnableState {
+		DISABLED,
+		AUTON,
+		TELEOP
+	}
+
+	public EnableState enableState = EnableState.DISABLED;
+
+	/**
+	 * The container for the robot. Contains subsystems, OI devices, and commands.
+	 */
+	public RobotContainer() {
+		INSTANCE = this;
+		driverController = new XboxController(0);
+		operatorController = new XboxController(1);
+		// Configure the button bindings
+		LiveWindow.disableAllTelemetry();
+		LiveWindow.setEnabled(false);
+
+		
+
+		manager = new SubsystemManager(0.02);
+
+		manager.setSubsystems();
+
+		configureButtonBindings();
+		configureAuton();
+
+
+	}
+
+	/**
+	 * Use this method to define your button->command mappings. Buttons can be created by
+	 * instantiating a {@link GenericHID} or one of its subclasses ({@link
+	 * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+	 * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+	 */
+	private void configureButtonBindings() {
+				
+	
+		//firstBall.and(secondBall).whenActive(new InstantCommand(() -> cargoManager.setWantedState(CargoManager.WantedState.SPINUP)));
+
+	}
+
+	private void configureAuton() {
+		
+
+	}
+
+	public static synchronized RobotContainer getInstance() {
+		if (INSTANCE == null) {
+			INSTANCE = new RobotContainer();
+		}
+		return INSTANCE;
+	}
+
+	public SendableChooser<Command> getAutonChooser() {
+		return autonChooser;
+	}
+
+	public void startSubsystemThreads(){
+		manager.start();
+	}
+
+	public void stopSubsystemThreads(){
+		manager.stop();
+	}
+
+	public void checkSubsystems() {
+		manager.checkSubsystems();
+	}
+
+	public void stopSubsystems() {
+		manager.stopSubsystems();
+	}
+
+}
---
+++ src/main/java/frc/robot/loops/Loop.java
@@ -0,0 +1,19 @@
+package frc.robot.loops;
+
+/**
+ * Interface for loops, which are routine that run periodically in the robot code (such as periodic
+ * gyroscope calibration, etc.)
+ * 
+ * FROM TEAM 27 RUSH
+ */
+public interface Loop {
+
+	void onStart(double timestamp);
+
+	void onLoop(double timestamp);
+
+	void onStop(double timestamp);
+
+	String getId();
+}
+
